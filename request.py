f411456805018cf639054646d73fd4111e219edd
roboxroz <roboxroz@gmail.com>
2023-11-06 19:46:51 +0800

    Resolved conflict with Templates branch

--- request.py
+++ request.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
 import matplotlib.pyplot as plt
 import plotly as pltly
+import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/', static_folder='templates')
+
+# Load dataset
+csv_file_path = 'datasets/model_output.csv'
+df = pd.read_csv(csv_file_path)
 
 @app.route('/')
 def index():
@@ -10,14 +15,22 @@ def index():
 
 @app.route('/get_time_level', methods=['POST'])
 def check_occupancy():
-    timing = request.form.get('time')
+    time = int(request.form.get('time'))
     level = request.form.get('level')
+    week = request.form.get('week')
+    day = int(request.form.get('day'))
     
     # Replace the following with the logic to fetch occupancy rate and generate visualizations
-    occupancy_rate = calculate_occupancy_rate(timing, level)
-    visualization = generate_visualization(occupancy_rate)
+    occupancy_rate = calculate_occupancy_rate(time, level)
+    #visualization = generate_visualization(occupancy_rate)
 
-    return render_template('overall_view.html', result=occupancy_rate, visualization=visualization)
+    # Filter the DataFrame based on the parameters, replace for more filters
+    filtered_df = df[(df['level'] == level) & (df['hour'] == time)]
+
+    # Calculate the total occupancy for the filtered data
+    total_occupancy = filtered_df['occupancy'].sum()
+
+    return render_template('floor_view.html', result=occupancy_rate, time = time, level = level, total_occupancy = total_occupancy, week=week, day=day)
 
 @app.route('/overall_view')
 def overall_view():
@@ -57,7 +70,7 @@ def floor_view(floor):
 
 def calculate_occupancy_rate(timing, level):
     # Replace this with the occupancy rate calculation logic
-    return f'Occupancy rate for Level {level} at {timing} is 60%'  # Replace with actual data
+    return f'Occupancy rate for Level {level} at {timing} is not sure'  # Replace with actual data
 
 def generate_visualization(occupancy_rate):
     # Replace this with your visualization generation logic using Matplotlib or Plotly
